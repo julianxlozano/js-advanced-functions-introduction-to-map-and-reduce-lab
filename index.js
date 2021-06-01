6af1cbe9c46371993922216db1c41a664e3fc8ab
julianxlozano <julianxlozano@gmail.com>
2021-06-01 14:41:03 -0500

    commit tree dd2c3c515349c756ec8f6cc1cba03bbc11f977db

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+
+function mapToNegativize(array){
+    let newArray = [];
+    for (let i = 0;i<array.length;i++){
+        newArray.push(array[i] * -1) 
+    };
+    return newArray
+};
+
+function mapToNoChange(array){
+    return array
+};
+
+function mapToDouble(array){
+    let newArray = [];
+    for (let i = 0;i<array.length;i++){
+        newArray.push(array[i] * 2) 
+    };
+    return newArray
+};
+
+function mapToSquare(array){
+    let newArray = [];
+    for (let i = 0;i<array.length;i++){
+        newArray.push(array[i] * array[i]) 
+    };
+    return newArray
+};
+
+function reduceToTotal(array,int=0){
+    let accumulator = int
+    for (let i = 0;i<array.length;i++){
+       accumulator += array[i]
+    };
+    return accumulator
+};
+
+function reduceToAllTrue(array){
+   return (array.includes(false) ? false : true);
+};
+
+function reduceToAnyTrue(array){
+    return (array.includes(true) ? true : false);
+ };
+
